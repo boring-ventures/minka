1905ad7ec77bdb73e5dbeb4cf1b5b41b362e5d4b
Santiago Sarabia <blvckcenturion@gmail.com>
2025-03-20 15:50:45 -0400

    feat:login modifications

--- src/components/auth/sign-in/components/sign-in-form.tsx
+++ src/components/auth/sign-in/components/sign-in-form.tsx
@@ -160,7 +160,7 @@ export function SignInForm() {
         <Button
           type="button"
           variant="outline"
-          className="flex items-center justify-center border rounded-full"
+          className="flex items-center justify-center border border-black rounded-md h-11"
           onClick={() => handleSocialSignIn("facebook")}
           disabled={!!socialLoading}
         >
@@ -172,12 +172,12 @@ export function SignInForm() {
         <Button
           type="button"
           variant="outline"
-          className="flex items-center justify-center border rounded-full"
+          className="flex items-center justify-center border border-black rounded-md h-11"
           onClick={() => handleSocialSignIn("google")}
           disabled={!!socialLoading}
         >
           <svg
-            className="h-5 w-5 mr-2"
+            className="h-5 w-5"
             viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg"
             aria-labelledby="googleIconTitle"
@@ -200,12 +200,14 @@ export function SignInForm() {
               d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"
             />
           </svg>
-          <span>{socialLoading === "google" ? "Cargando..." : "Google"}</span>
+          <span className="ml-2">
+            {socialLoading === "google" ? "Cargando..." : "Google"}
+          </span>
         </Button>
         <Button
           type="button"
           variant="outline"
-          className="flex items-center justify-center border rounded-full"
+          className="flex items-center justify-center border border-black rounded-md h-11"
           onClick={() => handleSocialSignIn("apple")}
           disabled={!!socialLoading}
         >
--- src/components/auth/sign-up/components/sign-up-form.tsx
+++ src/components/auth/sign-up/components/sign-up-form.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -165,7 +165,7 @@ export function SignUpForm() {
           <div className="relative">
             <button
               type="button"
-              className="flex items-center justify-between h-10 px-3 border border-r-0 rounded-l-md bg-white text-sm"
+              className="flex items-center justify-between h-11 px-3 border border-black border-r-0 rounded-l-md bg-white text-sm"
             >
               <div className="flex items-center">
                 <span className="mr-2">🇧🇴</span>
@@ -245,7 +245,7 @@ export function SignUpForm() {
           <div className="relative">
             <button
               type="button"
-              className="flex items-center justify-between h-10 px-3 border border-r-0 rounded-l-md bg-white text-sm"
+              className="flex items-center justify-between h-11 px-3 border border-black border-r-0 rounded-l-md bg-white text-sm"
             >
               <div className="flex items-center">
                 <span className="mr-2">🇧🇴</span>
@@ -257,7 +257,7 @@ export function SignUpForm() {
           <Input
             id="phone"
             {...form.register("phone")}
-            placeholder="Número de teléfono"
+            placeholder="Ingresa tu número de teléfono"
             className="flex-1 rounded-l-none"
           />
         </div>
@@ -352,7 +352,7 @@ export function SignUpForm() {
         <Button
           type="button"
           variant="outline"
-          className="flex items-center justify-center border rounded-full"
+          className="flex items-center justify-center border border-black rounded-md h-11"
           onClick={() => handleSocialSignIn("facebook")}
           disabled={!!socialLoading}
         >
@@ -364,12 +364,12 @@ export function SignUpForm() {
         <Button
           type="button"
           variant="outline"
-          className="flex items-center justify-center border rounded-full"
+          className="flex items-center justify-center border border-black rounded-md h-11"
           onClick={() => handleSocialSignIn("google")}
           disabled={!!socialLoading}
         >
           <svg
-            className="h-5 w-5 mr-2"
+            className="h-5 w-5"
             viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg"
             aria-labelledby="googleIconTitle"
@@ -392,12 +392,14 @@ export function SignUpForm() {
               d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"
             />
           </svg>
-          <span>{socialLoading === "google" ? "Cargando..." : "Google"}</span>
+          <span className="ml-2">
+            {socialLoading === "google" ? "Cargando..." : "Google"}
+          </span>
         </Button>
         <Button
           type="button"
           variant="outline"
-          className="flex items-center justify-center border rounded-full"
+          className="flex items-center justify-center border border-black rounded-md h-11"
           onClick={() => handleSocialSignIn("apple")}
           disabled={!!socialLoading}
         >
